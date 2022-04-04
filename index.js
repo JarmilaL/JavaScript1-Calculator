65bf80bc15c6926080f18bc9a095c9408a26ff9d
Jarmila Lenyiova <lenyiova.jarmila@gmail.com>
2022-04-04 20:45:41 +0200

    Adding Task 3

--- index.js
+++ index.js
@@ -51,3 +51,31 @@ Pokud to chcete zkusit bez dalsich napoved, nectete dal ;)
     v predchozim bode. Vyzkousime, jestli se nam do konzole vypisuje presne to, na co klikame (cislo nebo operator).
 5) Propojime obe funkce, ktere jsme naprogramovali tak, aby se to, na co klikame, vypisovalo do okynka kalkulacky.
 */
+
+/* Ukol 3
+-------------
+Urcite jste si vsimli, ze nase vypisovani do okynka je prozatim docela hloupe a umoznuje nam
+napsat treba takovy nesmysl jako 4 5 - = 3 + + 1 1 2 apod. Nevadi, To vyresime pozdeji. 
+Prozatim programujeme tzv. happy path, tzn. predpokladame, ze uzivatel je sikovny a klika tak, ze
+vytvari logicke priklady treba 2 + 2 = :) 
+
+Tak ted tyto jednoduche priklady nechame nas programek i vyresit. Konecne jdeme pocitat! :) 
+Prozatim dve jednociferna cisla ;)
+
+Cesta k reseni neni primocara. Neco naprogramujeme, pak zjistime, ze lepsi by bylo jine reseni, tak kod
+upravujeme. To je uplne normalni stav. Dulezite je snazit se delat logicke kroky a neskakat od jednoho
+k druhemu :) 
+
+1) Potrebujeme neco dalsiho z html nebo uz vsechno mame?
+2) Nachystame si funkci pro vypocet, protoze to je gro tohoto tasku :) Funkce bude opravdu jen pocitat
+    a vracet vysledek, takze by mela mit nejake parametry, s kterymi bude pracovat.
+3) Samotny priklad mame v 'okynku' kalkulacky v podobe textoveho retezce. Budeme tedy potrebovat 
+    funkci, ktera nam z toho Stringu vytahne jednotlive polozky, abychom s nimi mohli dal pracovat - treba
+    je predat funkci, ktera pocita.
+    String ma nektere vlastnosti pole - da se pres nej iterovat, da se vytahnout znak na urcitem indexu, ap.
+    A ma spoustu uzitecnych metod (metoda je funkce, ktera nalezi nejakemu objektu), diky kterym se
+    s nim da vselijak kouzlit. Doporucuji prozkoumat :) 
+    Ja jsem se rozhodla vyuzit metodu split().
+4) Pridam si na vhodne misto logiku toho, co se ma stat, kdyz kliknu na '=' 
+    (V podstate tim pospojuju to, co jsem si doted nachystala). No a je hotovo :) 
+*/
